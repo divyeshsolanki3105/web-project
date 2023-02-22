1f6c186691a3577dbe92d2480c593e1c77b75f88
divyesh solanki <you@example.com>
2023-02-22 15:56:05 +0530

    First Commit

---
+++ abc.js
@@ -0,0 +1,92 @@
+// var firstname=[];
+// for(var i=0; i<=firstname.length; i++){
+//     document.write("Javascript");
+// }
+// a()
+//console.log(b);
+
+// b();
+// console.log(x);
+
+// var y;
+// console.log("y:", y);
+// var x = 10;
+
+// var a = function () {
+//   console.log("func a");
+// };
+// console.log(a);
+
+// function b() {
+//   console.log("func b");
+// }
+// const fru=["banana","orange","mango"];
+// let text=fru.toString();
+// console.log(text);
+// var myArr=[1,2,3,4,5];
+// console.log(myArr.shift());
+// myArr.shift();
+// var a=[];
+// a.unshift(1);
+// a.unshift(22);
+// a.shift();
+// a.unshift(3,[4,5]);
+// a.shift();
+// a.shift();
+// console.log(a.shift());
+// const hero=["sd","df","f"];
+// let length=hero.length;
+// console.log(length);
+// const arr=[1,2,3,9,10,7,5,4,3];
+// const d=arr.filter(val=>val>=5);
+// // console.log(d);
+// const f=["or","app","ma","ba","app"];
+// let i=f.lastIndexOf("app");
+// console.log(i);
+// const a1=[1,2,3,4];
+// const a2=[4,5,6];
+// const sum=0;
+// var firstName=[];
+// for(var i=0; i<=firstName.length; i++){
+//     console.log("JS");
+// }
+// var numbers=[1,2,3,4];
+// const multipl=numbers.map((numbers)=>{
+//     return(numbers*5);
+// })
+// console.log(multipl);
+// var myArr=["foo","bar","baz"];
+// myArr[2];
+// console.log("2 is myArr");
+// var val=[4,5,6,7];
+// var ans=val.slice(1);
+// console.log(ans);
+// function oddsums(n){
+//     let total=0,result=[];
+//     for(let x=1; x<n; x++){
+//         let odd=2*x-1;
+//         total+=odd;
+//         result.push(total);
+//     }
+//     return result;
+// }
+// oddsums(5);
+// var a=[];
+//<br/>
+// a.unshift(5);
+
+// a.unshift(22);
+// <br/>
+// a.shift();
+// <br/>
+// a.unshift(3,[4,5]);
+// <br/>
+// a.shift();
+// <br/>
+// a.shift();
+// <br/>
+// a.shift();
+var a=[1,2,3];
+a[10]=99;
+console.log(a);
+console.log(a[6])
